ec4c4c98fbd0fb29216fa778c1eea98cbca8e049
Hassan <63836815+Hassan-jaffer@users.noreply.github.com>
2024-03-28 19:58:27 +0300

    can now move the cam left and right

--- Assets/MouseLook.cs
+++ Assets/MouseLook.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,8 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float mouseX  = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY  = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
