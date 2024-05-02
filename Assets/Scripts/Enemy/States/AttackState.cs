060bcc271cded0160221be963e0387a225a17ffb
Hassan <63836815+Hassan-jaffer@users.noreply.github.com>
2024-05-02 03:12:03 +0300

    attack state basic

--- Assets/Scripts/Enemy/States/AttackState.cs
+++ Assets/Scripts/Enemy/States/AttackState.cs
@@ -2,8 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackState : MonoBehaviour
+public class AttackState : BaseState
 {
+    private float moveTimer;
+    private float losePlayerTimer;
+
+    public override void Enter()
+    {
+    }
+
+    public override void Exit()
+    {
+    }
+
+    public override void Perform()
+    {
+    }
+
     // Start is called before the first frame update
     void Start()
     {
