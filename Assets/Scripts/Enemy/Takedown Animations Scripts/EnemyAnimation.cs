6c66a0ae8642084d7f9e04f3b46532e4c341b50c
Hasoooni2003 <Hasan.alshehabi2003@gmail.com>
2024-05-09 18:19:21 +0300

    Some improvements

--- Assets/Scripts/Enemy/Takedown Animations Scripts/EnemyAnimation.cs
+++ Assets/Scripts/Enemy/Takedown Animations Scripts/EnemyAnimation.cs
@@ -1,6 +1,7 @@
 using SojaExiles;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -17,13 +18,12 @@ public abstract class EnemyAnimation : MonoBehaviour
     protected bool isStaggered; //a boolean to check when the enemy reached critical state
     public Transform[] transferTargets; //for transfering the player to the correct position
     protected CharacterController playerController; //to disable the player movement during the animation
-    
     protected bool isAnimating = false; //to prevent multiple takedowns in the same place
     protected string nearestTargetName;
-    public Rigidbody[] ragdollRigidbodies; // Array of rigidbodies representing the enemy's body parts
-    public float launchForce = 100f; // The force applied to launch the enemy
+    [HideInInspector]public Rigidbody[] ragdollRigidbodies; // Array of rigidbodies representing the enemy's body parts
+    [HideInInspector]public Collider[] ragdollColliders; // Array of Colliders representing the enemy's body parts
 
-    protected bool isRagdollActive = false;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -33,7 +33,6 @@ public abstract class EnemyAnimation : MonoBehaviour
         playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController>();//get player component
         playerAnimator = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();//get player component
         enemyAnimator = GetComponent<Animator>();
-        
         SetRagdollActive(false);
     }
     protected abstract void Update();
@@ -102,7 +101,8 @@ public abstract class EnemyAnimation : MonoBehaviour
     {
         enemyAnimator.enabled = false;
         SetRagdollActive(true);
-        enabled = false;
+        Destroy(gameObject,10f);
+        //enabled = false;
     }
     /// <summary>
     /// Activate or deactivate the ragdoll component on the enemy body
@@ -115,13 +115,30 @@ public abstract class EnemyAnimation : MonoBehaviour
         {
             rb.isKinematic = !isActive;
         }
-
-        isRagdollActive = isActive;
+        foreach (Collider collider in ragdollColliders)
+        { 
+            collider.enabled = isActive; 
+        }
+        
     }
     private void Awake()
     {
         // Automatically populate the ragdollRigidbodies array with the rigidbody components of the enemy's body parts
         ragdollRigidbodies = GetComponentsInChildren<Rigidbody>();
+        ragdollColliders = GetComponentsInChildren<Collider>();
         
+    }
+    /// <summary>
+    /// Called by public methods in animation events to apply force
+    /// </summary>
+    /// <param name="force">force power to apply</param>
+    /// <param name="launchDirection">direction of force</param>
+    protected void addForce(float force, Vector3 launchDirection)
+    {
+        foreach (Rigidbody rb in ragdollRigidbodies)
+        {
+            rb.isKinematic = false;
+            rb.AddForce(launchDirection * force, ForceMode.Impulse);
+        }
     }
 }
