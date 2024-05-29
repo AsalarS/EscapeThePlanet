74a1cdc7b14eed0c765adaf260d9fbbdfa208d06
Hasoooni2003 <Hasan.alshehabi2003@gmail.com>
2024-05-29 20:56:53 +0300

    added a xp script (TO DO LATER)

---
+++ Assets/Scripts/ExperienceManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExperienceManager : MonoBehaviour
+{
+    public static ExperienceManager Instance;
+    public delegate void ExperienceChangeHandler(int amount);
+    public event ExperienceChangeHandler OnExperienceChange;
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    public void AddExperience(int amount)
+    {
+
+    }
+}
