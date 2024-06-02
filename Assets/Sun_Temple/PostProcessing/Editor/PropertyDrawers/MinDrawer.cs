17e2ab1aaa6ec5bf3250bbdf3ae2a97a9a6d89e2
Hassan <63836815+Hassan-jaffer@users.noreply.github.com>
2024-06-02 04:20:20 +0300

    test

--- Assets/Sun_Temple/PostProcessing/Editor/PropertyDrawers/MinDrawer.cs
+++ Assets/Sun_Temple/PostProcessing/Editor/PropertyDrawers/MinDrawer.cs
@@ -3,12 +3,12 @@ using UnityEngine.PostProcessing;
 
 namespace UnityEditor.PostProcessing
 {
-    [CustomPropertyDrawer(typeof(UnityEngine.MinAttribute))]
+    [CustomPropertyDrawer(typeof(MinAttribute))]
     sealed class MinDrawer : PropertyDrawer
     {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            UnityEngine.MinAttribute attribute = (UnityEngine.MinAttribute)base.attribute;
+            MinAttribute attribute = (MinAttribute)base.attribute;
 
             if (property.propertyType == SerializedPropertyType.Integer)
             {
